73c58c903365cce8406f9e99bcef916282be8b3b
alist7 <alisanati_007@yahoo.com>
2019-04-20 08:56:18 -0700

    fibonatchi_code

---
+++ fibonatchi.py
@@ -0,0 +1,15 @@
+first = 1
+second = 1
+limit = 28
+i = 1
+print(first)
+print(second)
+while i < limit:
+    a = first + second
+    first = second
+    second = a
+    i = i + 1
+    print(a)
+
+
+
